df6500d96ef60c1c314acb8f3394e4088eddb3d1
reyhanhmdani <rey7dan7@gmail.com>
2026-02-13 13:56:31 +0700

    fix filament image

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -11,7 +11,7 @@ class AdminSeeder extends Seeder
     public function run(): void
     {
         User::updateOrCreate(
-            ['email' => 'admin@sayfelfalah.sch.id'],
+            ['email' => ' '],
             [
                 'name' => 'Admin Sayf El Falah',
                 'password' => Hash::make('admin123'),
